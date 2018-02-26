5e8dda1d1869e984da65daf10af2043b0a20162e
Jullian Cockerell <jullian.cockerell@yahoo.com>
2018-02-26 16:55:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,11 +20,11 @@ function logWhisper(string)
 
 function sayHiToGrandma(string)
 {
-  if(string === lowercase)
+  if(string === string.toLowerCase())
   {
     return("I can't hear you!")
   }
-  if(string === uppercase)
+  if(string === string.toUpperCase())
   {
     return("YES INDEED!")
   }
